0ced5fdc0ae158dc11810e5521f0523d6a013360
Jarold <jarold@ecdesign.es>
2023-02-19 12:04:18 +0100

    fixed flot

--- core/templatetags/models_tags.py
+++ core/templatetags/models_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from apps.backend.models import Client
 
@@ -7,7 +8,7 @@ register = template.Library()
 
 User = get_user_model()
 
-@register.inclusion_tag('templatetags/bart_users.html')
+@register.inclusion_tag('templatetags/bart.html')
 def bart_users():
     data = []
     users = User.objects.filter(is_staff=False, is_superuser=False)
@@ -19,6 +20,24 @@ def bart_users():
             data.append([f"{ user.username }", users_clients ])
 
     context = {
+        'id': 'bart_users',
+        'title': _('data by users'),
+        'data':data
+    }
+    return context
+
+
+@register.inclusion_tag('templatetags/bart.html')
+def bart_companies():
+    data = []
+    clients = Client.objects.all()
+    company_chelion = clients.filter(business=1).count()
+    data.append(['Chelion Iberia', company_chelion ])
+    company_iberian = clients.filter(business=2).count()
+    data.append(['Iberian Trade Europe', company_iberian ])
+    context = {
+        'id': 'bart_companies',
+        'title': _('data by companies'),
         'data':data
     }
     return context
