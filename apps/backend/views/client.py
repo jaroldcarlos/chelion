e9b6a77d4ad49d70e49ff18575efc0f5d20187d0
Jarold Meléndez <jarold@ecdesign.es>
2023-03-06 11:14:56 +0100

    Fixed list user

--- apps/backend/views/client.py
+++ apps/backend/views/client.py
@@ -103,10 +103,12 @@ def clients_to_csv(request):
 
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = f'attachment; filename="{filename}"'
-    writer = csv.writer(response)
-
+    writer = csv.writer(
+        response,
+        delimiter=';',
+        quotechar='"',
+        quoting=csv.QUOTE_MINIMAL)
     fields = Client.get_fields()
-
     writer.writerow(fields)
 
     for obj in queryset:
